d33cbacd81eefa2e2d426d7de128c77b6261a6e5
SanjalaR <144262889+SanjalaR@users.noreply.github.com>
2024-02-03 11:59:50 +0530

    Indian States Guessing game
    Part of Indian states Guessing game

--- game.py
+++ game.py
@@ -3,29 +3,27 @@ import turtle
 import pandas
 
 screen = turtle.Screen()
-screen.title("Name the States")
+screen.title("India States Game")
 
-image = "blank_states_img.gif"
+image = "new.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-# To get x and y coords of all states
-# def get_mouse_click_coor(x, y):
-#     print(x, y)
+# def get_mouse_coords(x, y):
+#     print(x)
+#     print(y)
 #
 #
-# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.onscreenclick(get_mouse_coords)
 # turtle.mainloop()
 
-data = pandas.read_csv("50_states.csv")
-
-data_list = data["state"].to_list()
-print(data_list)
+data = pandas.read_csv("states.csv")
+data_list = data["State"].to_list()
 score = 0
 answers = []
 
-for i in range (50):
-    answer = screen.textinput(f"{score}/50 States Correct", "Enter the name of a state: ").title()
+for i in range(28):
+    answer = screen.textinput(title=f"{score}/28 States Correct", prompt="Enter the name of a state: ").title()
 
     if answer == "Exit":
         break
@@ -33,17 +31,20 @@ for i in range (50):
     if answer in data_list:
         answers.append(answer)
         score += 1
-        states = data[data.state == answer]
+        row = data[data.State == answer]
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
-        t.goto(int(states.x), int(states.y))
-        t.write(states.state.item())
+        t.goto(int(row.x), int(row.y))
+        t.write(row.State.item())
 
-learn = [state for state in data_list if state not in answers]
-# for state in data_list:
-#     if state not in answers:
-#         learn.append(state)
+learn = []
+for state in data_list:
+    if state not in answers:
+        learn.append(state)
+
 new = pandas.DataFrame(learn)
 new.to_csv("to_learn.csv")
 print(new)
+
+# screen.exitonclick()
