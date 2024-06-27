abf8245257b4a6f7cfde43ed9f56e0037912c713
SanjalaR <144262889+SanjalaR@users.noreply.github.com>
2024-06-27 18:59:00 +0530

    login authorization page

---
+++ day61/main.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.fields import EmailField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap5
+
+
+class MyForm(FlaskForm):
+    email = EmailField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label='Login')
+
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+app = Flask(__name__)
+app.secret_key = 'ssssssss'
+bootstrap = Bootstrap5(app)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = MyForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@email.com' and form.password.data == '12345678':
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+    return render_template('login.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
